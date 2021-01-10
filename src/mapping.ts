4b651c6614e2e2f70a7c6682c51cf85db7938c2c
Jenny Pollack <jennypollack3@gmail.com>
2021-01-10 17:10:55 +0100

    initial subgraph setup

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,30 +1,21 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, log } from "@graphprotocol/graph-ts"
 import { Contract, Registered, NewOwner } from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+import { Entry } from "../generated/schema"
+
+// id: ID!
+// functionName: String!
+// functionSig: String!
 
 export function handleRegistered(event: Registered): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  let entry = Entry.load(event.params.method.toString())
+  if (entry == null) {
+    entry = new Entry(event.params.method.toString())
+    entry.functionName = event.params.method.toString()
+    entry.functionSig = event.params.signature.toHex()
+    entry.save()
   }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.creator = event.params.creator
-  entity.signature = event.params.signature
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
 
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
@@ -32,19 +23,9 @@ export function handleRegistered(event: Registered): void {
   // entity back to the store. Fields that were not set or unset remain
   // unchanged, allowing for partial updates to be applied.
 
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.totalSignatures(...)
-  // - contract.owner(...)
-  // - contract.entries(...)
-  // - contract.register(...)
-}
 
-export function handleNewOwner(event: NewOwner): void {}
+  // let contract = Contract.bind(event.address)
+
+  // log.debug('total sigs {}', [contract.totalSignatures()])
+
+}
